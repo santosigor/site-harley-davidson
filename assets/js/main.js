733c59114fd05942c0aec1481b35b9623b38005d
Igor <santosigor013@gmail.com>
2018-03-13 22:09:28 -0300

    Criando a estrutura

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  // back to top
+
+  $('.back-to-top').on('click', function (e) {
+      e.preventDefault();
+      $('html,body').animate({ scrollTop: 0}, 700);
+  });
+
+  // Fixed
+
+  $(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 450) {
+      $('.back-to-top').addClass('fixed');
+    } else {
+      $('.back-to-top').removeClass('fixed');
+    }
+  }).scroll();
+
+  // End: back to top
+
+});
