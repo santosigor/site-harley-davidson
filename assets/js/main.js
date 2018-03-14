0615d0d4cb243edfbee82e54b3fc1ad3f1ef8498
Igor dos Santos <igor.santos@fiap.com.br>
2018-03-14 14:44:20 -0300

    desenvolvendo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+  // Carousel
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+      }
+   });
+
   // back to top
 
   $('.back-to-top').on('click', function (e) {
