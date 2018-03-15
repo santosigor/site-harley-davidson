213b0d83ad10991c07fa753ccb7b13f63db11299
Igor <santosigor013@gmail.com>
2018-03-15 04:15:53 -0300

    desenvolvendo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
 
-  // Carousel
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-      }
-   });
-
   // back to top
 
   $('.back-to-top').on('click', function (e) {
